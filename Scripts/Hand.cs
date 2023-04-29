bb6ef508f34304de5b19e387e73370ba8f2627c5
Antti Haavikko <antti.haavikko@gmail.com>
2023-04-29 18:52:48 +0300

    Add hand visuals

--- Scripts/Hand.cs
+++ Scripts/Hand.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Godot;
 using Godot.Collections;
@@ -10,6 +11,17 @@ public partial class Hand : StaticBody2D
     [Export] private NodePath target;
     [Export] private StaticBody2D collisionShape;
     [Export] private Bell bell;
+    [Export] private Node2D rotatePivot;
+    [Export] private Node2D armPos;
+    [Export] private Line2D grabLine;
+
+    private Vector2 offset;
+    private Node2D grabbed;
+
+    public override void _Ready()
+    {
+        grabLine.Hide();
+    }
 
     public override void _Input(InputEvent @event)
     {
@@ -18,7 +30,7 @@ public partial class Hand : StaticBody2D
 
         if (!mouseEvent.Pressed)
         {
-            pin.NodeB = null;
+            Release();
             return;
         }
         
@@ -27,6 +39,8 @@ public partial class Hand : StaticBody2D
 
     public void Release()
     {
+        grabbed = null;
+        grabLine.Hide();
         pin.NodeB = null;
     }
 
@@ -44,12 +58,28 @@ public partial class Hand : StaticBody2D
         {
             match.TryGetValue("collider", out var coll);
             bell.Cancel();
-            pin.NodeB = ((Node2D)coll).GetPath();
+            grabbed = (Node2D)coll;
+            pin.NodeB = grabbed.GetPath();
+
+            offset = grabbed.ToLocal(GlobalPosition);
+
+            grabLine.Show();
         }
     }
 
     public override void _Process(double delta)
     {
         Position = GetGlobalMousePosition();
+        var dir = armPos.GlobalPosition - GlobalPosition;
+        rotatePivot.Rotation = dir.Angle() + Mathf.Pi * 0.5f;
+
+        if (grabbed != default)
+        {
+            grabLine.Points = new[]
+            {
+                grabLine.ToLocal(GlobalPosition),
+                grabLine.ToLocal(grabbed.ToGlobal(offset))
+            };
+        }
     }
 }
